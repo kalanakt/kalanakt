12b500f1dfdb85073ecc1b210b1f294bd464e784
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-10-31 17:01:12 +0530

    Update #introduction.py

--- #introduction.py
+++ #introduction.py
@@ -1,8 +1,8 @@
-# you can Fork and edit this read me file if you want to(fork name is should be your username ***)
+# you can Fork and edit this read me file if you want to
 
 # follow this steps:
 
-  #1. fork repo (don't forget to hit star button)
+  #1. fork repo {fork name is should be your username ***}(don't forget to hit star button)
   #2. replace your github link for https://github.com/kalanakt
   #3. replace your github user name for kalanakt
   #4. repalce your social media link for my links

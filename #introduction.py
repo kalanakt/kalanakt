15794b472d7278d3b9bbd0aad1cf6e94fa12613a
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-10-31 15:23:09 +0530

    Create #introduction.py

---
+++ #introduction.py
@@ -0,0 +1,16 @@
+# you can Fork and edit this read me file if you want to
+
+# follow this steps:
+
+  #1. fork repo (don't forget to hit star button)
+  #2. replace your github link for https://github.com/kalanakt
+  #3. replace your github user name for kalanakt
+  #4. repalce your social media link for my links
+  #5. replace your whatsapp number for mine.
+  #6. change gif.video if you want to , get gif link from https://giphy.com
+  #7. you can also replace gif to picture , 
+        # mathord :  #open issues ( left on <code> button)
+                     #copy picture to issues
+                     #after that replace line .. to given text link
+        
+   
